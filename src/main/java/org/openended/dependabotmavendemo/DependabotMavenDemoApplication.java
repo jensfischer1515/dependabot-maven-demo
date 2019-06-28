73beab2d05f53da3e4f5e6196b5a7749fc285593
Jens Fischer <jens.fischer@otto.de>
2019-06-28 14:17:32 +0200

    initial import

---
+++ src/main/java/org/openended/dependabotmavendemo/DependabotMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package org.openended.dependabotmavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabotMavenDemoApplication.class, args);
+	}
+
+}
